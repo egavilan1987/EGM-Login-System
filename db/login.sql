dad42544affda47f779125808483ad51cdbabc83
egavilan1987 <enmanuelgc@gmail.com>
2019-02-28 07:41:45 -0400

    Initial commit

---
+++ db/login.sql
@@ -0,0 +1,20 @@
+CREATE DATABASE login;
+
+USE login;
+
+CREATE TABLE employees(
+				id_user INT AUTO_INCREMENT,
+
+				user_fullname VARCHAR(50),
+				user_email VARCHAR(50),
+				user_passworld VARCHAR(50),
+				user_email VARCHAR(50),
+				user_email VARCHAR(50),
+
+
+				created_date DATETIME,
+				updated_date DATETIME,
+
+				PRIMARY KEY(id_user)
+
+					);
